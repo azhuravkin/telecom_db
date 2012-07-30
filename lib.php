f4e8a38b3e784de1b8c78b181f2966dcb140b47c
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-07-30 21:19:21 +0300

    Formatting into lib.php

--- lib.php
+++ lib.php
@@ -10,57 +10,57 @@ if (($uri != "/db/login.php") && ($uri != "/db/logout.php"))
 </head>
 <style type = "text/css">
 h3 {
-	text-align: center;
-	border-color: #AAAAAA;
-	border-style: double;
-	font-family : Arial, sans-serif;
-	font-size: 14px;
+    text-align: center;
+    border-color: #AAAAAA;
+    border-style: double;
+    font-family : Arial, sans-serif;
+    font-size: 14px;
 }
 h4 {
-	font-family : Arial, sans-serif;
-	font-size: 13px;
+    font-family : Arial, sans-serif;
+    font-size: 13px;
 }
 input {
-	background-color: #ffffff;
-	border-bottom: #808080 1px solid;
-	border-left: #808080 1px solid;
-	border-right: #808080 1px solid;
-	border-top: #808080 1px solid;
-	color: #000000;
-	font-family: Arial, sans-serif;
-	font-size: 11px;
+    background-color: #ffffff;
+    border-bottom: #808080 1px solid;
+    border-left: #808080 1px solid;
+    border-right: #808080 1px solid;
+    border-top: #808080 1px solid;
+    color: #000000;
+    font-family: Arial, sans-serif;
+    font-size: 11px;
 }
 input.text {
-	width: 100%;
+    width: 100%;
 }
 table.border {
-	border-style: solid;
-	border-color: #AAAAAA;
-	border-width: 1px;
+    border-style: solid;
+    border-color: #AAAAAA;
+    border-width: 1px;
 }
 table.small {
-	background: #CCCCCC;
+    background: #CCCCCC;
 }
 th {
-	white-space: nowrap;
-	background: #E9E9E9;
-	font-family: Arial, sans-serif;
-	font-size: 12px;
+    white-space: nowrap;
+    background: #E9E9E9;
+    font-family: Arial, sans-serif;
+    font-size: 12px;
 }
 td {
-	white-space: nowrap;
-	background: #FFFFFF;
-	font-family: Arial, sans-serif;
-	font-size: 11px;
+    white-space: nowrap;
+    background: #FFFFFF;
+    font-family: Arial, sans-serif;
+    font-size: 11px;
 }
 a {
-	color: #0055aa;
-	text-decoration: none;
-	font-size: 12px;
+    color: #0055aa;
+    text-decoration: none;
+    font-size: 12px;
 }
 a:hover {
-	color: #3399ee;
-	text-decoration: none;
+    color: #3399ee;
+    text-decoration: none;
 }
 </style>
 <body>
@@ -69,42 +69,45 @@ a:hover {
 <td><a href="/db">На главную</a></td>
 <td align="right">
 <?php
-if (isset($_SESSION['valid_user']))
-	print "<a href='/db/logout.php'>Выход (" . $_SESSION['valid_user'] . ")</a>";
-else
-	print "<a href='/db/login.php'>Вход</a>";
+if (isset($_SESSION['valid_user'])) {
+    print "<a href='/db/logout.php'>Выход (" . $_SESSION['valid_user'] . ")</a>";
+} else {
+    print "<a href='/db/login.php'>Вход</a>";
+}
 ?>
 </td></tr>
 </table>
 <?php
 function init_db() {
-	$db_hostname = "localhost";
-	$db_username = "telecom";
-	$db_password = "telecom123";
-	$db_name = "telecom";
+    $db_hostname = "localhost";
+    $db_username = "telecom";
+    $db_password = "telecom123";
+    $db_name = "telecom";
 
-	if (!mysql_connect($db_hostname, $db_username, $db_password)) {
-		print "<h4><font color='red'>Невозможно подключиться к серверу mysql...</font></h4>\n";
-		print "</body>\n</html>";
-		exit;
-	}
+    if (!mysql_connect($db_hostname, $db_username, $db_password)) {
+	print "<h4><font color='red'>Невозможно подключиться к серверу mysql...</font></h4>\n";
+	print "</body>\n</html>";
+	exit;
+    }
 
-	if (!mysql_select_db($db_name)) {
-		print "<h4><font color='red'>Невозможно использовать базу $db_name...</font></h4>\n";
-		print "</body>\n</html>";
-		exit;
-	}
-	mysql_query("SET NAMES 'utf8'");
+    if (!mysql_select_db($db_name)) {
+	print "<h4><font color='red'>Невозможно использовать базу $db_name...</font></h4>\n";
+	print "</body>\n</html>";
+	exit;
+    }
+
+    mysql_query("SET NAMES 'utf8'");
 }
 
 function nextID($query) {
-	$result = mysql_query($query);
-	$max = mysql_fetch_array($result);
-	return $max[0] + 1;
+    $result = mysql_query($query);
+    $max = mysql_fetch_array($result);
+
+    return $max[0] + 1;
 }
 
 function goHome() {
-	print '<meta http-equiv="Refresh" content="1; URL=/db/">&nbsp;
+    print '<meta http-equiv="Refresh" content="1; URL=/db/">&nbsp;
 <div align="center"><h4><font color="red">Вы не можете просматривать эту страницу!</font></h4>';
 }
 
