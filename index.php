868ff125d8973787144e056484a49b0209740b5e
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-07-31 12:07:41 +0300

    Remove checking for valid_user in index.php

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
     session_start();
     include "lib.php";
-
-    if (isset($_SESSION['valid_user'])) {
-	print "<h3>Выберите базу данных:</h3>\n";
-	print "<table>\n\t<tr><td><b><a href='spr/'>Справочник номеров АТС</a></b></td></tr>\n";
-	print "\t<tr><td><b><a href='kross/'>Кросс АТС Hicom-300</a><b></td></tr>\n";
-	print "\t<tr><td><b><a href='pult_d/'>Цифровые пульты</a><b></td></tr>\n";
-	print "\t<tr><td><b><a href='pult_a/'>Аналоговые пульты</a><b></td></tr>\n";
-//	print "\t<tr><td><b><a href='sign/'>Сигнализация</a><b></td></tr>\n";
-//	print "\t<tr><td><b><a href='gts/'>Телефоны ГТС</a><b></td></tr>\n";
-//	print "\t<tr><td><b><a href='other/'>Прочая информация</a><b></td></tr>\n";
-	print "</table>\n";
-    }
 ?>
+<h3>Выберите базу данных:</h3>
+<table>
+    <tr><td><b><a href='spr/'>Справочник номеров АТС</a></b></td></tr>
+    <tr><td><b><a href='kross/'>Кросс АТС Hicom-300</a><b></td></tr>
+    <tr><td><b><a href='pult_d/'>Цифровые пульты</a><b></td></tr>
+    <tr><td><b><a href='pult_a/'>Аналоговые пульты</a><b></td></tr>
+    <tr><td><b><a href='sign/'>Сигнализация</a><b></td></tr>
+    <tr><td><b><a href='gts/'>Телефоны ГТС</a><b></td></tr>
+    <tr><td><b><a href='other/'>Прочая информация</a><b></td></tr>
+</table>
 </body>
 </html>
