ee4ccf08625b3ce206fbd5f49abd7352db4d108b
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-07-31 09:31:28 +0300

    Show message if user already logged in

--- login.php
+++ login.php
@@ -1,28 +1,31 @@
 <?php
-session_start();
-include("lib.php");
+    session_start();
+    include("lib.php");
 
-print "<h3>Вход:</h3><br>";
+    print "<h3>Вход:</h3><br>";
 
-if (isset($_POST['username']) and isset($_POST['password'])) {
-    // Если пользователь попытался зарегистрироваться
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    init_db();
-    $query = "SELECT * FROM auth WHERE username = '$username' AND password = md5('$password')";
-    $result = mysql_query($query);
-    if (mysql_num_rows($result) > 0) {
-	// Если пользователь найден в базе данных,
-	// регистрируем его идентификатор
-	$access = mysql_fetch_assoc($result);
-	$_SESSION['valid_user'] = $username;
-	$_SESSION['writable'] = $access['writable'];
-	print '<meta http-equiv="Refresh" content="1; URL=/db/">&nbsp;<div align="center"><h4>Вы вошли.</h4>';
-    } else {
-	print '<meta http-equiv="Refresh" content="1; URL=/db/login.php">&nbsp;<div align="center"><h4>Неудачная попытка входа</h4>';
+    if (isset($_SESSION['valid_user'])) {
+	die("<div align='center'><h4>Вы уже авторизованы как " . $_SESSION['valid_user'] . ".</h4></div>");
+    } elseif (isset($_POST['username']) and isset($_POST['password'])) {
+	// Если пользователь попытался зарегистрироваться
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	init_db();
+	$query = "SELECT * FROM auth WHERE username = '$username' AND password = md5('$password')";
+	$result = mysql_query($query);
+	if (mysql_num_rows($result) > 0) {
+	    // Если пользователь найден в базе данных,
+	    // регистрируем его идентификатор
+	    $access = mysql_fetch_assoc($result);
+	    $_SESSION['valid_user'] = $username;
+	    $_SESSION['writable'] = $access['writable'];
+	    die("<meta http-equiv='Refresh' content='1; URL=/db/'>&nbsp;<div align='center'><h4>Вы вошли.</h4>\n");
+	} else {
+	    print "<div align='center'><h4><font color='red'>Неудачная попытка входа. Попробуйте ещё раз.</font></h4>";
+	}
     }
-} else {
-    print '<table class="border" cellspacing="0" align="center">
+?>
+<table class="border" cellspacing="0" align="center">
 <tr>
 <td>
     <table>
@@ -34,6 +37,4 @@ if (isset($_POST['username']) and isset($_POST['password'])) {
 </td></tr>
 </table>
 </body>
-</html>';
-}
-?>
+</html>
