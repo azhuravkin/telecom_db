92070890f5248643722afcda45a4fe96d6aa27e9
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-08-02 11:29:22 +0300

    Calculate md5sum of data in edit_menu.php

--- kross/edit_menu.php
+++ kross/edit_menu.php
@@ -3,17 +3,18 @@ session_start();
 include "../lib.php";
 
 if ($_SESSION['writable'] == 'Y') {
+	$string = "";
+	$query = "SELECT * FROM `dlu` ORDER BY `sort`";
+	$result = mysql_query($query);
+
 	print "<h3>Редактирование списка DLU:</h3>
 <table class='small' cellspacing='1'>
 <th colspan='3'>Название DLU:</th>
 <th>Удалить:</th>
 <form action='update_dlu_menu.php' method='post'>";
 
-	$query = "SELECT * FROM dlu ORDER BY sort";
-	$result = mysql_query($query);
-
 	$i = 0;
-	while ($row = mysql_fetch_assoc($result)) {
+	while ($row = mysql_fetch_array($result)) {
 		print "\n<tr>\n\t<td>DLU N-</td>\n\t<td>";
 		print "<input type='text' name='sort[$i]' size='2' value='";
 		print $row['sort'];
@@ -28,12 +29,18 @@ if ($_SESSION['writable'] == 'Y') {
 		print "'></td>\n";
 		print "</tr>";
 		$i++;
+		// Сохраняем все данные в переменную для подсчёта контрольной суммы
+		$string .= concat($row);
 	}
+	// Вычисляем контрольную сумму редактируемых данных
+	print "\n<input type='hidden' name='md5sum' value='".md5($string)."'>\n";
+
 	print '</table><p>
 <table width="1%">
 <tr>
 <td><input type="submit" value="Сохранить"></form></td>
 <form align="left" action="add_dlu.php" method="post">
+
 <td><input type="submit" value="Добавить DLU"></td>
 </form>
 </tr>
