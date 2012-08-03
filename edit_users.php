8174d753955ac931abda41b6d308c7540ad1cc49
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-08-03 17:18:06 +0300

    Put authID as index of array

--- edit_users.php
+++ edit_users.php
@@ -13,14 +13,14 @@
 <th>Пользователь:</th><th>Пароль:</th><th>Редактирование:</th><th>Администратор:</th><th>Удалить:</th>
 </tr>';
 	while ($row = mysql_fetch_array($result)) {
-	    print "\n<tr>\n\t<input type='hidden' name='authID' value='".$row['authID']."'>";
-	    print "\n\t<td width='30%'>".$row['username']."</td>";
-	    print "\n\t<td width='20%'><input type='password' name='password' value=''></td>";
-	    print "\n\t<td width='17%' align='center'><input type='checkbox' name='writable'";
+	    $i = $row['authID'];
+	    print "\n<tr>\n\t<td width='20%'><input type='text' name='username[$i]' value='".$row['username']."'></td>";
+	    print "\n\t<td width='20%'><input type='password' name='password[$i]' value=''></td>";
+	    print "\n\t<td width='20%' align='center'><input type='checkbox' name='writable[$i]'";
 	    if ($row['writable'] == 'Y') print " checked";
-	    print "></td>\n\t<td width='17%' align='center'><input type='checkbox' name='admin'";
+	    print "></td>\n\t<td width='20%' align='center'><input type='checkbox' name='admin[$i]'";
 	    if ($row['admin'] == 'Y') print " checked";
-	    print "></td>\n\t<td width='17%' align='center'><input type='checkbox' name='delete'></tr>";
+	    print "></td>\n\t<td width='20%' align='center'><input type='checkbox' name='delete[$i]'></tr>";
 	}
 	print '</table><p>
 <table width="1%">
