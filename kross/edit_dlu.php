6efb2f4d5377b76c14c1b2c0473ffeb601d9e406
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-08-04 16:28:25 +0300

    Set dluID as index of array in form edit_dlu.php

--- kross/edit_dlu.php
+++ kross/edit_dlu.php
@@ -5,66 +5,56 @@ $dluID = $_GET['dluID'];
 include "../lib.php";
 
 if ($_SESSION['writable'] == 'Y') {
-	$string = "";
-	$query = "SELECT * FROM `dlu` WHERE `dluID` = '$dluID'";
-	$result = mysql_query($query);
-	$row = mysql_fetch_assoc($result);
+    $string = "";
+    $query = "SELECT * FROM `dlu` WHERE `dluID` = '$dluID'";
+    $result = mysql_query($query);
+    $row = mysql_fetch_assoc($result);
 
-	print "<h3>DLU N-";
-	print $row['sort'];
-	print " - ";
-	print $row['name'];
-	print "</h3>";
+    print "<h3>DLU N-";
+    print $row['sort'];
+    print " - ";
+    print $row['name'];
+    print "</h3>";
 
-	print "<table class='small' cellspacing='1' width='100%'>\n";
-	print "<th>Пара</th>\n";
-	print "<th>Тел.</th>\n";
-	print "<th>Пульт</th>\n";
-	print "<th>Сигн.</th>\n";
-	print "<th>Pen</th>\n";
-	print "<th>Кросс</th>\n";
-	print "<th>Абонент</th>\n";
+    print "<table class='small' cellspacing='1' width='100%'>\n";
+    print "<tr>\n\t<th>Пара</th>\n\t<th>Тел.</th>\n\t<th>Пульт</th>\n\t<th>Сигн.</th>";
+    print "\n\t<th>Pen</th>\n\t<th>Кросс</th>\n\t<th>Абонент</th>\n</tr>\n";
 
-	$query = "SELECT * FROM `para` WHERE `dluID` = '$dluID' ORDER BY `para`";
-	$result = mysql_query($query);
+    $query = "SELECT * FROM `para` WHERE `dluID` = '$dluID' ORDER BY `para`";
+    $result = mysql_query($query);
 
-	print "<form action='update_dlu.php' method='post'>\n";
-	print "<input type='hidden' name='dluID' value='$dluID'>\n";
+    print "<form action='update_dlu.php' method='post'>\n";
+    print "<input type='hidden' name='dluID' value='$dluID'>\n";
 
-	for ($i = 0; $row = mysql_fetch_array($result); $i++) {
-		print "<tr>\n";
-		print "\t<input type='hidden' name='paraID[$i]' value='";
-		print $row['paraID'];
-		print "'>\n\t<td align='center' width='5%'>";
-		print $row['para'];
-		print "</td>\n\t<td width='5%'>";
-		print "<input type='text' class='text' name='tel[$i]' value='";
-		print $row['telephone'];
-		print "'></td>\n\t<td width='7%'>";
-		print "<input type='text' class='text' name='pult[$i]' value='";
-		print $row['pult'];
-		print "'></td>\n\t<td width='7%'>";
-		print "<input type='text' class='text' name='sign[$i]' value='";
-		print $row['sign'];
-		print "'></td>\n\t<td width='7%'>";
-		print "<input type='text' class='text' name='pen[$i]' value='";
-		print $row['pen'];
-		print "'></td>\n\t<td width='7%'>";
-		print "<input type='text' class='text' name='kross[$i]' value='";
-		print $row['kross'];
-		print "'></td>\n\t<td width='62%'>";
-		print "<input type='text' class='text' name='abonent[$i]' value='";
-		print $row['abonent'];
-		print "'></td>\n</tr>";
-		// Сохраняем все данные в переменную для подсчёта контрольной суммы
-		$string .= concat($row);
-	}
-	// Вычисляем контрольную сумму редактируемых данных
-	print "<input type='hidden' name='md5sum' value='".md5($string)."'>\n";
+    while ($row = mysql_fetch_array($result)) {
+	$paraID = $row['paraID'];
 
-	print "</table><p><input type='submit' value='Сохранить'></p>\n</form>";
+	print "<tr>\n\t<td align='center' width='5%'>";
+	print $row['para'];
+	print "</td>\n\t<td width='5%'><input type='text' class='text' name='tel[$paraID]' value='";
+	print $row['telephone'];
+	print "'></td>\n\t<td width='7%'><input type='text' class='text' name='pult[$paraID]' value='";
+	print $row['pult'];
+	print "'></td>\n\t<td width='7%'><input type='text' class='text' name='sign[$paraID]' value='";
+	print $row['sign'];
+	print "'></td>\n\t<td width='7%'><input type='text' class='text' name='pen[$paraID]' value='";
+	print $row['pen'];
+	print "'></td>\n\t<td width='7%'><input type='text' class='text' name='kross[$paraID]' value='";
+	print $row['kross'];
+	print "'></td>\n\t<td width='62%'><input type='text' class='text' name='abonent[$paraID]' value='";
+	print $row['abonent'];
+	print "'></td>\n</tr>\n";
+
+	// Сохраняем все данные в переменную для подсчёта контрольной суммы
+	$string .= concat($row);
+    }
+
+    // Вычисляем контрольную сумму редактируемых данных
+    print "<input type='hidden' name='md5sum' value='".md5($string)."'>\n";
+
+    print "</table><p><input type='submit' value='Сохранить'></p>\n</form>";
 } else {
-	goHome();
+    goHome();
 }
 ?>
 </body>
--- kross/update_dlu.php
+++ kross/update_dlu.php
@@ -3,34 +3,32 @@ session_start();
 include "../lib.php";
 
 if ($_SESSION['writable'] == 'Y') {
-	$dluID = $_POST['dluID'];
+    $dluID = $_POST['dluID'];
 
-	// Сравниваем контрольные суммы данных перед изменениями
-	if ($_POST['md5sum'] != md5_count("SELECT * FROM `para` WHERE `dluID` = '$dluID' ORDER BY `para`")) {
-		print '<div align="center"><h4><font color="red">Информация в этом DLU была обновлена другим пользователем!!!</font></h4>';
-	} else {
-		for ($i = 0; $i <= 99; $i++) {
-			$paraID = $_POST['paraID'][$i];
-			$tel = trim($_POST['tel'][$i]);
-			$pult = trim($_POST['pult'][$i]);
-			$sign = trim($_POST['sign'][$i]);
-			$pen = trim($_POST['pen'][$i]);
-			$kross = trim($_POST['kross'][$i]);
-			$abonent = trim($_POST['abonent'][$i]);
+    // Сравниваем контрольные суммы данных перед изменениями
+    if ($_POST['md5sum'] != md5_count("SELECT * FROM `para` WHERE `dluID` = '$dluID' ORDER BY `para`")) {
+	print '<div align="center"><h4><font color="red">Информация в этом DLU была обновлена другим пользователем!!!</font></h4>';
+    } else {
+	foreach ($_POST['tel'] as $paraID => $number) {
+	    $tel = trim($number);
+	    $pult = trim($_POST['pult'][$paraID]);
+	    $sign = trim($_POST['sign'][$paraID]);
+	    $pen = trim($_POST['pen'][$paraID]);
+	    $kross = trim($_POST['kross'][$paraID]);
+	    $abonent = trim($_POST['abonent'][$paraID]);
 
-			// Вносим изменения в таблицу para
-			$query = "UPDATE `para` SET `telephone` = '$tel', `pult` = '$pult', `sign` = '$sign',
-			`pen` = '$pen', `kross` = '$kross', `abonent` = '$abonent' WHERE `paraID` = '$paraID'";
+	    // Вносим изменения в таблицу para
+	    $query = "UPDATE `para` SET `telephone` = '$tel', `pult` = '$pult', `sign` = '$sign',
+		`pen` = '$pen', `kross` = '$kross', `abonent` = '$abonent' WHERE `paraID` = '$paraID'";
 
-			mysql_query($query) or die ("Query failed");
-		}
-
-		print '<meta http-equiv="Refresh" content="1; URL=/db/kross/dlu.php?dluID=' . $dluID . '">&nbsp;
-<div align="center"><h4>Изменения были сохранены.</h4>';
+	    mysql_query($query) or die ("Query failed");
 	}
 
+	print '<meta http-equiv="Refresh" content="1; URL=/db/kross/dlu.php?dluID=' . $dluID . '">&nbsp;
+<div align="center"><h4>Изменения были сохранены.</h4>';
+    }
 } else {
-	goHome();
+    goHome();
 }
 ?>
 </body>
