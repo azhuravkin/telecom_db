5539b9a465a824a05feb2cca1de426b362a31311
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-08-10 11:11:58 +0300

    Submit arrays from form in edit_razdel.php

--- spr/edit_razdel.php
+++ spr/edit_razdel.php
@@ -5,103 +5,63 @@
 
     if ($_SESSION['writable'] == 'Y') {
 	// Получить уникальные podrazdelID из данного раздела
-	$query1 = "SELECT DISTINCT podrazdelID AS count FROM service
-	WHERE service.razdelID = '$razdelID' ORDER BY service.podrazdelID";
+	$query1 = "SELECT DISTINCT `podrazdelID` AS `podrazdelID` FROM `service`
+	    WHERE `service`.`razdelID` = '$razdelID' ORDER BY `service`.`podrazdelID`";
 	$result1 = mysql_query($query1);
-	while ($row1 = mysql_fetch_assoc($result1)) {
-	    $podrazdelID = $row1['count'];
-	    $query2 = "SELECT name FROM podrazdel WHERE podrazdel.podrazdelID = '$podrazdelID'";
-	    $result2 = mysql_query($query2);
-	    $row2 = mysql_fetch_assoc($result2);
 
-	    print "<h3><table align='center' width='1%'><form action='update_podrazdel_name.php' method='post'>";
-	    print "<tr>\n<td><input type='text' size='40' name='podrazdelName' value='";
-	    print $row2['name'];
-	    print "'></td>\n<td>";
-	    print "<input type='hidden' name='podrazdelID' value='";
-	    print $podrazdelID;
-	    print "'>\n<input type='hidden' name='razdelID' value='$razdelID'>";
-	    print "<input type='submit' value='Сохранить'></td></form>";
-	    print "<form action='del_podrazdel.php' method='get'>";
-	    print "<td><input type='submit' value='Удалить подраздел'></td>\n";
-	    print "<input type='hidden' name='podrazdelID' value='";
-	    print $podrazdelID;
-	    print "'><input type='hidden' name='razdelID' value=" . $razdelID . "></tr></form></table></h3>\n";
+	while ($row1 = mysql_fetch_array($result1)) {
+	    $podrazdelID = $row1['podrazdelID'];
+	    $query2 = "SELECT * FROM `podrazdel` WHERE `podrazdelID` = '$podrazdelID'";
+	    $result2 = mysql_query($query2);
+	    $row2 = mysql_fetch_array($result2);
+	    $name = $row2['name'];
+
+	    print "<h3><table align='center' width='1%'><form action='update_podrazdel.php' method='post'>\n";
+	    print "<input type='hidden' name='razdelID' value='$razdelID'>\n";
+	    print "<tr>\n\t<td><input type='text' size='40' name='podrazdelName[$podrazdelID]' value='$name'></td>\n";
+	    print "\t<td><input type='checkbox' name='del_podrazdel[$podrazdelID]'></td>\n";
+	    print "\t<td>Удалить подраздел</td>\n</tr>\n</table></h3>\n";
 
 	    print "<table class='small' cellspacing='1' width='100%'>\n";
-	    print "<tr><th>Название</th>";
-	    print "<th>Ф.И.О.</th>";
-	    print "<th>Номер</th></tr>";
+	    print "<tr><th>Название</th><th>Ф.И.О.</th><th>Номер</th></tr>";
 
-	    $query3 = "SELECT service.serviceID, service.name, service.comment, number.numberID, number.telephone
-		FROM service, number WHERE number.serviceID = service.serviceID
-		AND service.podrazdelID = '$podrazdelID' ORDER BY service.serviceID";
+	    $query3 = "SELECT `service`.`serviceID`, `service`.`name`, `service`.`comment`, `number`.`numberID`, `number`.`telephone`
+		FROM `service`, `number` WHERE `number`.`serviceID` = `service`.`serviceID`
+		AND `service`.`podrazdelID` = '$podrazdelID' ORDER BY `service`.`serviceID`";
 	    $result3 = mysql_query($query3);
 
-	    $oldID = 0;
-	    while ($row3 = mysql_fetch_assoc($result3)) {
+	    for ($oldID = 0; $row3 = mysql_fetch_array($result3);) {
 		$newID = $row3['serviceID'];
-		    if ($newID != $oldID) {
-			print "<TR>\n\t<form action='update_service_name.php' method='get'>";
-			print "<TD width='33%'>";
-			print "\n<table width='100%'><tr><td>\n";
-			print "<input type='text' class='text' name='serviceName' value='";
-			print $row3['name'];
-			print "'>\n<input type='hidden' name='serviceID' value='";
-			print $row3['serviceID'];
-			print "'><input type='hidden' name='razdelID' value='$razdelID'></td>\n";
-			print "<td width='1%'><input type='submit' value='Сохранить'>";
-			print "</td></tr></table></TD></form>\n";
-			print "\t<TD width='33%'>";
-			print "<table width='100%'><tr><form action='update_service_comment.php' method='get'>\n<td>\n";
-			print "<input type='text' class='text' size='50' name='serviceComment' value='";
-			print $row3['comment'];
-			print "'>\n<input type='hidden' name='serviceID' value='";
-			print $row3['serviceID'];
-			print "'><input type='hidden' name='razdelID' value='$razdelID'></td>\n";
-			print "<td width='1%'>\n<input type='submit' value='Сохранить'>";
-			print "</td></form></table></TD>\n\t<TD width='33%'>";
-			print "<table width='1%'><tr><form action='update_service_number.php' method='get'>\n<td>\n";
-			print "<input type='text' size='9' name='number' value='";
-			print $row3['telephone'];
-			print "'>\n<input type='hidden' name='numberID' value='";
-			print $row3['numberID'];
-			print "'><input type='hidden' name='razdelID' value='$razdelID'></td>\n";
-			print "</td><td>\n<input type='submit' value='Сохранить'></form></td>";
-			print "<form action='del_number.php' method='get'>\n<td>\n";
-			print "<input type='hidden' name='numberID' value='";
-			print $row3['numberID'];
-			print "'><input type='hidden' name='razdelID' value='$razdelID'>\n";
-			print "<input type='submit' value='Удалить'></form></td>";
-			print "<form action='add_number.php' method='get'>\n<td>\n";
-			print "<input type='text' name='telephone' size='9'></td><td>";
-			print "<input type='hidden' name='serviceID' value='";
-			print $row3['serviceID'];
-			print "'><input type='hidden' name='razdelID' value='$razdelID'>\n";
-			print "<input type='submit' value='Добавить'></form></td></tr></table>\n";
+		$serviceID = $row3['serviceID'];
+		$name = $row3['name'];
+		$comment = $row3['comment'];
+		$numberID = $row3['numberID'];
+		$telephone = $row3['telephone'];
 
-			$oldID = $newID;
-		    } else {
-			print "<table width='1%'><tr><form action='update_service_number.php' method='get'>\n<td>\n";
-			print "<input type='text' size='9' name='number' value='";
-			print $row3['telephone'];
-			print "'>\n<input type='hidden' name='numberID' value='";
-			print $row3['numberID'];
-			print "'></td><td>\n<input type='submit' value='Сохранить'></form></td>\n";
-			print "<form action='del_number.php' method='get'>\n<td>\n";
-			print "<input type='hidden' name='numberID' value='";
-			print $row3['numberID'];
-			print "'><input type='hidden' name='razdelID' value='$razdelID'>\n";
-			print "<input type='submit' value='Удалить'></form></td></tr></table>";
+		if ($newID != $oldID) {
+		    if ($oldID) {
+			print "\n</td>\n</tr>\n";
 		    }
+		    print "<tr>\n\t<td width='50%'><input type='text' class='text' name='serviceName[$serviceID]' value='$name'></td>\n";
+		    print "\t<td width='30%'><input type='text' class='text' size='50' name='serviceComment[$serviceID]' value='$comment'></td>\n";
+		    print "\t<td width='20%'><table><tr>\n\t\t<td><input type='text' size='9' name='number[$numberID]' value='$telephone'></td>\n";
+		    print "\t\t<td><input type='checkbox' name='del_number[$numberID]'></td>\n\t\t<td>Удалить</td>\n";
+		    print "\t\t<td><input type='text' class='text' size='9' name='new_number[$serviceID]'></td></tr></table>";
+
+		    $oldID = $newID;
+		} else {
+		    print "\n\t\t<table><tr>\n\t\t<td><input type='text' size='9' name='number[$numberID]' value='$telephone'></td>\n";
+		    print "\t\t<td><input type='checkbox' name='del_number[$numberID]'></td>\n\t\t<td>Удалить</td></tr></table>";
 		}
-		print "</table></TD>\n</TR>\n</TABLE>&nbsp;\n";
-		print "<form align='left' action='add_service.php' method='get'>";
-		print "<input type='hidden' name='razdelID' value='$razdelID'>";
-		print "<input type='hidden' name='podrazdelID' value='$podrazdelID'>";
-		print "<input type='submit' value='Добавить службу'></form>";
+	    }
+
+	    print "</td>\n</tr>\n</table>\n<p><input type='submit' value='Сохранить'></form>\n";
+	    print "<form action='add_service.php' method='post'>";
+	    print "<input type='hidden' name='razdelID' value='$razdelID'>";
+	    print "<input type='hidden' name='podrazdelID' value='$podrazdelID'>";
+	    print "<input type='submit' value='Добавить службу'></form></p>";
 	}
-	print "<p><form align='left' action='add_podrazdel.php' method='get'>";
+	print "<p><form action='add_podrazdel.php' method='post'>";
 	print "<input type='hidden' name='razdelID' value='$razdelID'>";
 	print "<input type='submit' value='Добавить подраздел'></form></p>\n";
     } else {
