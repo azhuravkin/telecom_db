615d9e548094f686f0e082237d21840af4b017bc
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-08-11 17:09:43 +0300

    Select podrazdel in add_service.php

--- spr/add_service.php
+++ spr/add_service.php
@@ -3,24 +3,33 @@
 
     if ($_SESSION['writable'] == 'Y') {
 	$razdelID = $_POST['razdelID'];
-	$podrazdelID = $_POST['podrazdelID'];
 
-	if ((empty($_GET["name"])) or (empty($_GET["number"]))) {
-	    print '<form method="get">
-<h3>Добавление новой службы:</h3>
-<table class="small" cellspacing="1" width="65%">
-<th>Название</th>
+	if (empty($_POST["name"])) {
+	    print "<form method='post'>\n<h3><select name='podrazdelID'>";
+
+	    // Получить название данного подраздела
+	    $query = "SELECT * FROM `podrazdel` WHERE `razdelID` = '$razdelID' ORDER BY `podrazdelID`";
+	    $result = mysql_query($query);
+
+	    while ($row = mysql_fetch_array($result)) {
+		$podrazdelID = $row['podrazdelID'];
+		$name = $row['name'];
+		print "\t<option value='$podrazdelID'>$name</option>\n";
+	    }
+
+	    print "</select></h3>
+<table class='small' cellspacing='1' width='65%'>
+<th>Название*</th>
 <th>Ф.И.О.</th>
 <th>Номер</th>
 <tr>
-<td width="50%"><input type="text" class="text" name="name"></td>
-<td width="30%"><input type="text" class="text" name="comment"></td>
-<td width="20%"><input type="text" class="text" name="number" size="9"></td>
+<td width='50%'><input type='text' class='text' name='name'></td>
+<td width='30%'><input type='text' class='text' name='comment'></td>
+<td width='20%'><input type='text' class='text' name='number' size='9'></td>
 </tr></table>
-<p><input type="submit" value="Добавить"></p>
-<input type="hidden" name="razdelID" value="'.$razdelID.'">
-<input type="hidden" name="podrazdelID" value="'.$podrazdelID.'">
-</form>';
+<p><input type='submit' value='Добавить'></p>
+<input type='hidden' name='razdelID' value='$razdelID'>
+</form>";
 	} else {
 	    $razdelID = $_POST['razdelID'];
 	    $podrazdelID = $_POST['podrazdelID'];
@@ -32,20 +41,16 @@
 	    $query = "SELECT MAX(`serviceID`) FROM `service`";
 	    $serviceID = nextID($query);
 
-	    // Ищем свободный numberID
-	    $query = "SELECT MAX(`numberID`) FROM `number`";
-	    $numberID = nextID($query);
-
 	    // Вставляем данные в таблицу service
-	    $query = "INSERT INTO service VALUES ('$serviceID','$name','$comment','$podrazdelID')";
-	    mysql_query($query) or die ("Query failed");
+	    $query = "INSERT INTO `service` VALUES ('$serviceID', '$name', '$comment', '$podrazdelID')";
+	    mysql_query($query) or die ("Query failed 1");
 
 	    // Вставляем данные в таблицу number
-	    $query = "INSERT INTO number VALUES ('$numberID','$number','$serviceID')";
-	    mysql_query($query) or die ("Query failed");
+	    $query = "INSERT INTO `number` VALUES (NULL, '$number', '$serviceID')";
+	    mysql_query($query) or die ("Query failed 2");
 
-	    print "<meta http-equiv=\"Refresh\" content=\"1; URL=/db/spr/edit_razdel.php?razdelID=" . $razdelID . "\">";
-	    print '&nbsp;<div align="center"><h4>Новая служба добавлена.</h4>';
+	    print "<meta http-equiv=\"Refresh\" content=\"1; URL=/db/spr/edit_razdel.php?razdelID=".$razdelID."\">";
+	    print "&nbsp;<div align=\"center\"><h4>Новая служба добавлена.</h4>\n";
 	}
     } else {
 	goHome();
