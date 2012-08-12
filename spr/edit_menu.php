9884c1585b5fa55890aa1920ef72bcd6d54f17a8
Alexander Zhuravkin <alexandr.zhuravkin@gmail.com>
2012-08-12 09:06:54 +0300

    Calculate md5sum of data in spr/edit_menu.php

--- spr/edit_menu.php
+++ spr/edit_menu.php
@@ -2,6 +2,7 @@
     include "../header.php";
 
     if ($_SESSION['writable'] == 'Y') {
+	$string = "";
 	print "<h3>Редактирование разделов справки:</h3>\n";
 	print "<table class='small' cellspacing='1'>\n<tr>\n\t";
 	print "<th>Название раздела:</th>\n\t<th>Удалить:</th>\n</tr>\n";
@@ -18,7 +19,13 @@
 	    print "'></td>\n\t<td align='center'><input type='checkbox' name=\"del_razdelID[$razdelID]\" value='";
 	    print $row['razdelID'];
 	    print "'></td>\n</tr>\n";
+
+	    // Сохраняем все данные в переменную для подсчёта контрольной суммы
+	    $string .= concat($row);
 	}
+
+	// Вычисляем контрольную сумму редактируемых данных
+	print "<input type='hidden' name='md5sum' value='".md5($string)."'>\n";
 
 	print '</table><p>
 <table width="1%">
